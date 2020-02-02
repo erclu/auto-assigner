fdc4af43d41a6b56772c8c6155dc5e591013f53f
erclu <30255227+erclu@users.noreply.github.com>
2020-02-02 17:08:02 +0100

    chore: lint test files with another project, add coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,14 @@
 module.exports = {
+  // TODO are tsc sourcemap needed?
   clearMocks: true,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
   moduleFileExtensions: ["js", "ts"],
   testEnvironment: "node",
   testMatch: ["**/*.test.ts"],
